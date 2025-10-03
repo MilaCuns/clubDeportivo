62ad44698d0196b7ce6579d3ccae3704bcf7d8da
LucreLucre <lucrevigo@gmail.com>
2025-10-03 11:14:14 -0300

    Primer commit

---
+++ app/src/main/java/com/example/clubdeportivo/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.clubdeportivo
+
+import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener{
+            val intent = Intent(this, MenuPrincipalActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
