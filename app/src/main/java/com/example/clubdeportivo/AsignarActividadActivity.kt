41089ea3daefe60ff649dc3590595f1c4ca29f2c
Camila Cuns <cunscamiladaniela@gmail.com>
2025-10-05 10:00:29 -0300

    Update AsignarActividadActivity.kt

--- app/src/main/java/com/example/clubdeportivo/AsignarActividadActivity.kt
+++ app/src/main/java/com/example/clubdeportivo/AsignarActividadActivity.kt
@@ -1,20 +1,32 @@
 package com.example.clubdeportivo
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class AsignarActividadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_asignar_actividad)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val grupoActividades = findViewById<RadioGroup>(R.id.grupoActividades)
+        val etDni = findViewById<EditText>(R.id.etDni)
+        val btnAsignar = findViewById<Button>(R.id.btnAsignar)
+        val btnPagar = findViewById<Button>(R.id.btnPagar)
+
+        // Botón para asignar actividad
+        btnAsignar.setOnClickListener {
+            val intent = Intent(this, AsignarActividadGuardadoActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Botón para ir a la pantalla de pago
+        btnPagar.setOnClickListener {
+            val intent = Intent(this, PagarCuotaActivity::class.java)
+            startActivity(intent)
         }
     }
 }
