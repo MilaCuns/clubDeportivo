0429a6032b0e2e62dc7d8999345f568ea9e66b40
Camila Cuns <cunscamiladaniela@gmail.com>
2025-10-05 18:11:24 -0300

    Update FacturaDigitalCuotaSocioActivity.kt

--- app/src/main/java/com/example/clubdeportivo/FacturaDigitalCuotaSocioActivity.kt
+++ app/src/main/java/com/example/clubdeportivo/FacturaDigitalCuotaSocioActivity.kt
@@ -3,21 +3,29 @@ package com.example.clubdeportivo
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class FacturaDigitalCuotaSocioActivity : AppCompatActivity() {
+class ListarSociosCuotaVencidaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_factura_digital_cuota_socio)
+        setContentView(R.layout.activity_listar_socios_cuota_vencida)
 
-        val btnVerCarnet = findViewById<Button>(R.id.btnVerCarnet)
-        btnVerCarnet.setOnClickListener {
-            val intent = Intent(this, CarnetSocioActivity::class.java)
+       
+        val btnPagarCuota = findViewById<Button>(R.id.btnPagarCuota)
+        btnPagarCuota.setOnClickListener {
+           
+            val intent = Intent(this, PagarCuotaActivity::class.java)
             startActivity(intent)
         }
+
+       
+        val btnVerCarnet = findViewById<Button>(R.id.btnVerCarnet)
+        btnVerCarnet.setOnClickListener {
+            
+            val intent = Intent(this, VerCarnetActivity::class.java)
+            startActivity(intent)
+        }
+
+        
     }
 }
